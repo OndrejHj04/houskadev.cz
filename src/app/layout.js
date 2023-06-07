ced7f56e279d706914cc0c151bcda7654ad509ac
OndrejHj04 <ondra.hajku@seznam.cz>
2023-06-07 17:12:35 +0200

    seo fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,6 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Houska dev",
-  description: "Vývojářská skupina Houska dev",
-};
 
 export default function RootLayout({ children }) {
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -7,18 +7,28 @@ import Facility from "./pages/Facility";
 import Gallery from "./pages/Gallery";
 import ContactPage from "./pages/ContactPage";
 import Footer from "./pages/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <MenuPage />
-      <div className="overflow-y-scroll overflow-x-hidden flex-1">
-        <IntroPage />
-        <Facility />
-        <Activities />
-        <Gallery />
-        <ContactPage />
-        <Footer />
+    <div>
+      <Head>
+        <title>Moje stránka</title>
+        <meta
+          name="google-site-verification"
+          content="sA_Y9uRtjtkf9Zq8tEQhNuJI5Qy94RCzROdc8oJUUoE"
+        />
+      </Head>
+      <div className="flex flex-col">
+        <MenuPage />
+        <div className="overflow-y-scroll overflow-x-hidden flex-1">
+          <IntroPage />
+          <Facility />
+          <Activities />
+          <Gallery />
+          <ContactPage />
+          <Footer />
+        </div>
       </div>
     </div>
   );
