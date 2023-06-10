6f81ef671a7866d4a6144dfb0c4575ae236597e5
OndrejHj04 <ondra.hajku@seznam.cz>
2023-06-10 17:37:58 +0200

    sticky header

--- src/app/pages/Menu.jsx
+++ src/app/pages/Menu.jsx
@@ -1,10 +1,8 @@
-import styles from "../styles/menu.module.css";
-import "../styles/menu.css";
 import { GrTechnology } from "react-icons/gr";
 
 function MenuPage() {
   return (
-    <div className="shadow-xl border-b-2 border-gray-200 flex">
+    <div className="shadow-xl border-b-2 border-gray-200 flex sticky top-0 z-50 bg-white">
       <GrTechnology size={64} className="relative" />
       <div className="text-2xl flex items-center">
         <h1>
