227efda74da308b361d6a23ff82db062dbeb1016
OndrejHj04 <ondra.hajku@seznam.cz>
2023-06-11 12:56:56 +0200

    deploy

--- src/app/pages/Menu.jsx
+++ src/app/pages/Menu.jsx
@@ -1,6 +1,6 @@
 function MenuPage() {
   return (
-    <div className="shadow-xl border-b-2 border-gray-200 flex sticky top-0 z-50 bg-white h-16"></div>
+    <div className="shadow-xl border-b-2 border-gray-200 flex sticky top-0 z-50 bg-white h-16" />
   );
 }
 export default MenuPage;
