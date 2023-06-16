153178fad0eb777e9dd3d46b41912ddbf3beb93a
OndrejHj04 <ondra.hajku@seznam.cz>
2023-06-16 22:28:56 +0200

    cool footer

--- src/app/pages/Footer.jsx
+++ src/app/pages/Footer.jsx
@@ -1,19 +1,44 @@
+"use client";
 import Image from "next/image";
-import styles from "../styles/footer.module.css";
+import "../styles/footer.scss";
 
 export default function Footer() {
   return (
-    <div
-      className={`${styles.footer} border-2 m-2 p-5 rounded-3xl flex justify-between items-center`}
-    >
+    <div className={`footer p-5 flex justify-between text-gray-300`}>
       <div>
         <h2 className="font-bold text-3xl">Houska DEV</h2>
         <p>protože není nic staršího než včerejší weby...</p>
       </div>
-      <div className="flex">
-        <img src="gmail.png" className="h-16" />
-        <img src="facebook.png" className="h-16 mx-2" />
-        <img src="disc.png" className="h-16" />
+      <div className="flex flex-col">
+        <h4 className="text-center">Kontakt</h4>
+        <div className="grid grid-cols-3 gap-1.5">
+          <div className="cursor-pointer">
+            <Image
+              src="/fb-ico.png"
+              height={36}
+              width={36}
+              alt="facebook icon"
+            />
+          </div>
+
+          <div className="cursor-pointer">
+            <Image
+              src="/gmail-ico.png"
+              height={36}
+              width={36}
+              alt="gmail icon"
+            />
+          </div>
+
+          <div className="cursor-pointer">
+            <Image
+              src="/disc-ico.png"
+              height={36}
+              width={36}
+              alt="discord icon"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
